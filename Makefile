18fad68b178da01b8e9231a85611b120c767ee86
Jacob Preston <prestja@gmail.com>
2020-03-22 18:31:26 -0500

    Separates file IO from insertion

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 INPUT_PATH		:=	input.txt
 SORT_PATH		:= 	sort.txt
 BALANCE_FACTOR	:=	1
-N				:=	1000000
+N				:=	100000
 
 default:
 	g++ -Ofast main.cpp avl.cpp -o avl.out 
--- main.cpp
+++ main.cpp
@@ -12,26 +12,39 @@
 #include <chrono>
 #include <stdio.h>
 #include <unistd.h>
+#include <vector>
 // project include
 #include "avl.h"
+
+std::vector<int> buffer;
+
 
 void parse(avl_tree* tree, char* path) 
 {
 	std::ifstream file (path);
 	std::string line;
-	while (getline(file, line)) 
-	{
-		std::stringstream ss (line);
-		int next;
-		ss >> next;
-		//std::cout << "Inserting " << next << std::endl;
-		avl* root = tree->getRoot();
-		avl* inserted = tree->insert(root, next);
+	
+	int next;
 
-		root = tree->getRoot();
-		if (root == NULL) {
-			tree->setRoot(inserted);
-		}
+	auto start = std::chrono::steady_clock::now();
+	while (file >> next) 
+	{
+		buffer.push_back(next);
+	}
+	auto end = std::chrono::steady_clock::now();
+	std::cout << "Took " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << " ns to parse file" << std::endl;
+
+	// inserts the first element so that root is never null
+	avl* root = new avl();
+	root->d = buffer[0];
+	root->l = NULL;
+	root->r = NULL;
+	tree->setRoot(root);
+
+	// inserts all remaining elements
+	for (int i = 1; i < buffer.size(); i++) 
+	{
+		tree->insert(tree->getRoot(), buffer[i]);
 	}
 }
 
@@ -42,15 +55,16 @@ int main (int argc, char** argv)
 	}
 	int balFac = 1;
 	balFac = std::stoi(argv[2]);
-	std::cout << "balance factor is " << balFac << std::endl;
 	// create a new tree and parse from the given input file, then perform a preorder traversal
 	avl_tree* tree = new avl_tree(balFac);
 
 	// parse tree and insert all elements from input file
-	auto i_start = std::chrono::steady_clock::now();
+	
+	//auto i_start = std::chrono::steady_clock::now();
 	parse(tree, argv[1]);
-	auto i_end = std::chrono::steady_clock::now();
-	std::cout << "Insertion: " << std::chrono::duration_cast<std::chrono::milliseconds>(i_end - i_start).count() << "ms" << std::endl;
+	//auto i_end = std::chrono::steady_clock::now();
+	//std::cout << "Insertion: " << std::chrono::duration_cast<std::chrono::milliseconds>(i_end - i_start).count() << "ms" << std::endl;
+	
 	/*
 	// find any leaf node to consider worst-case search time
 	auto f_start = std::chrono::steady_clock::now();
